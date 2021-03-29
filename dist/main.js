05a1a53cbcc30cb69b8afd4f35880cb40e107be3
Shea Cronin <shealcronin@gmail.com>
2021-03-29 19:49:03 -0400

    Move classes to nav-menus.js and create objects in index.js

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// Fn to toggle class.\nfunction toggleClass(el, cls) {\n    // Decide if complete class needs to be removed or added.\n    const action = el.classList.contains(cls) ? 'remove' : 'add';\n    // Remove/add the class.\n    el.classList[action](cls);\n}\n\nclass MenuItem {\n    constructor(el, name, index) {\n        this.el = el;\n        this.name = name;\n        this.index = index;\n        this.measuredDimension = 'height';\n        this._distanceFromStart;\n    }\n    get distanceFromStart() {\n        const measurement = parseInt(\n            window.getComputedStyle(this.el)[this.measuredDimension]\n        );\n        return measurement * (this.index + 1);\n    }\n    toggleDisplay(isVisible) {\n        if (isVisible) {\n            this.hide();\n        } else {\n            this.show();\n        }\n        this.triggerFade();\n    }\n    show() {\n        this.el.style.transform = `translate(0, ${this.distanceFromStart}px)`;\n    }\n    hide() {\n        this.el.style.transform = 'translate(0, 0)';\n    }\n    triggerFade() {\n        if (\n            !this.el.classList.contains('fade-in') &&\n            !this.el.classList.contains('fade-out')\n        ) {\n            this.el.classList.add('fade-in');\n        } else {\n            toggleClass(this.el, 'fade-in');\n            toggleClass(this.el, 'fade-out');\n        }\n    }\n}\n\nclass MobileMenuItem extends MenuItem {\n    constructor(el, name, index) {\n        super(el, name, index);\n        this.measuredDimension = 'width';\n    }\n    show() {\n        this.el.style.transform = `translate(${this.distanceFromStart}px, 0)`;\n    }\n}\n\nclass Menu {\n    constructor(titleEl, colorMode, itemsArray) {\n        this.items = itemsArray || [];\n        this.titleEl = titleEl;\n        this.titleEl.addEventListener('click', () => this.toggleMenuItems());\n        this._colorMode = colorMode;\n        this.itemsVisible = false;\n    }\n    get colorMode() {\n        return this._colorMode;\n    }\n    set colorMode(mode) {\n        // Remove spaces and standardize case of string.\n        const uniformMode = mode.replace(/[\\W_]/g, '').toLowerCase();\n        if (uniformMode !== 'monochrome' && uniformMode !== 'hues') {\n            alert('Invalid color mode. Please choose \"monochrome\" or \"hues\".');\n            return;\n        } else {\n            this._colorMode = uniformMode;\n        }\n    }\n    setZIndices() {\n        let z = this.items.length;\n        this.titleEl.style.zIndex = z + 1;\n        this.items.forEach((item) => {\n            item.el.style.zIndex = z;\n            z--;\n        });\n    }\n    setFilters() {\n        if (this.colorMode === 'monochrome') {\n            let bright = 0.9;\n            let sat = 1.1;\n            for (let i = 0; i < this.items.length; i++) {\n                this.items[i].el.style.filter =\n                    `brightness(${bright}) ` + `saturate(${sat})`;\n                bright -= 0.1;\n                sat += 0.1;\n            }\n        } else if (this.colorMode === 'hues') {\n            let n = 45;\n            for (let i = 0; i < this.items.length; i++) {\n                this.items[i].el.style.filter = `hue-rotate(${n}deg)`;\n                n += 45;\n            }\n        } else {\n            console.log('Please set a color mode.');\n        }\n    }\n    toggleMenuItems() {\n        this.items.forEach((item) => {\n            item.toggleDisplay(this.itemsVisible);\n        });\n        this.itemsVisible = !this.itemsVisible;\n    }\n}\n\n// Store all instances of menu class.\nconst allMenus = document.querySelectorAll('.menu');\n\nallMenus.forEach((menuEl) => {\n    const title = menuEl.querySelector('.menu-title');\n    // Create objects for each menu.\n    const menu = new Menu(title, 'hues');\n    const items = [...menuEl.querySelectorAll('.menu-item')];\n    items.forEach((itemEl) => {\n        // Create objects for each menu item.\n        const ItemClass = itemEl.classList.contains('mobile-menu-item')\n            ? MobileMenuItem\n            : MenuItem;\n        const item = new ItemClass(\n            itemEl,\n            itemEl.textContent,\n            items.indexOf(itemEl)\n        );\n        console.log(item);\n        menu.items.push(item);\n    });\n    // Set the z index style for each item.\n    menu.setZIndices();\n    menu.setFilters();\n});\n\n\n//# sourceURL=webpack://nav-menus/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nav_menus__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav-menus */ \"./src/nav-menus.js\");\n\n\n// Store all instances of menu class.\nconst allMenus = document.querySelectorAll('.menu');\n\nallMenus.forEach((menuEl) => {\n    const title = menuEl.querySelector('.menu-title');\n    // Create objects for each menu.\n    const menu = new _nav_menus__WEBPACK_IMPORTED_MODULE_0__.Menu(title, 'hues');\n    const items = [...menuEl.querySelectorAll('.menu-item')];\n    items.forEach((itemEl) => {\n        // Create objects for each menu item.\n        const ItemClass = itemEl.classList.contains('mobile-menu-item')\n            ? _nav_menus__WEBPACK_IMPORTED_MODULE_0__.MobileMenuItem\n            : _nav_menus__WEBPACK_IMPORTED_MODULE_0__.MenuItem;\n        const item = new ItemClass(\n            itemEl,\n            itemEl.textContent,\n            items.indexOf(itemEl)\n        );\n        menu.items.push(item);\n    });\n    // Set the z index style for each item.\n    menu.setZIndices();\n    menu.setFilters();\n});\n\n\n//# sourceURL=webpack://nav-menus/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/nav-menus.js":
+/*!**************************!*\
+  !*** ./src/nav-menus.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"MenuItem\": () => (/* binding */ MenuItem),\n/* harmony export */   \"MobileMenuItem\": () => (/* binding */ MobileMenuItem),\n/* harmony export */   \"Menu\": () => (/* binding */ Menu)\n/* harmony export */ });\n// Helper fn to toggle class.\nfunction toggleClass(el, cls) {\n    // Decide if complete class needs to be removed or added.\n    const action = el.classList.contains(cls) ? 'remove' : 'add';\n    // Remove/add the class.\n    el.classList[action](cls);\n}\n\nclass MenuItem {\n    constructor(el, name, index) {\n        this.el = el;\n        this.name = name;\n        this.index = index;\n        this.measuredDimension = 'height';\n        this._distanceFromStart;\n    }\n    get distanceFromStart() {\n        const measurement = parseInt(\n            window.getComputedStyle(this.el)[this.measuredDimension]\n        );\n        return measurement * (this.index + 1);\n    }\n    toggleDisplay(isVisible) {\n        if (isVisible) {\n            this.hide();\n        } else {\n            this.show();\n        }\n        this.triggerFade();\n    }\n    show() {\n        this.el.style.transform = `translate(0, ${this.distanceFromStart}px)`;\n    }\n    hide() {\n        this.el.style.transform = 'translate(0, 0)';\n    }\n    triggerFade() {\n        if (\n            !this.el.classList.contains('fade-in') &&\n            !this.el.classList.contains('fade-out')\n        ) {\n            this.el.classList.add('fade-in');\n        } else {\n            toggleClass(this.el, 'fade-in');\n            toggleClass(this.el, 'fade-out');\n        }\n    }\n}\n\nclass MobileMenuItem extends MenuItem {\n    constructor(el, name, index) {\n        super(el, name, index);\n        this.measuredDimension = 'width';\n    }\n    show() {\n        this.el.style.transform = `translate(${this.distanceFromStart}px, 0)`;\n    }\n}\n\nclass Menu {\n    constructor(titleEl, colorMode, itemsArray) {\n        this.items = itemsArray || [];\n        this.titleEl = titleEl;\n        this.titleEl.addEventListener('click', () => this.toggleMenuItems());\n        this._colorMode = colorMode;\n        this.itemsVisible = false;\n    }\n    get colorMode() {\n        return this._colorMode;\n    }\n    set colorMode(mode) {\n        // Remove spaces and standardize case of string.\n        const uniformMode = mode.replace(/[\\W_]/g, '').toLowerCase();\n        if (uniformMode !== 'monochrome' && uniformMode !== 'hues') {\n            alert('Invalid color mode. Please choose \"monochrome\" or \"hues\".');\n            return;\n        } else {\n            this._colorMode = uniformMode;\n        }\n    }\n    setZIndices() {\n        let z = this.items.length;\n        this.titleEl.style.zIndex = z + 1;\n        this.items.forEach((item) => {\n            item.el.style.zIndex = z;\n            z--;\n        });\n    }\n    setFilters() {\n        if (this.colorMode === 'monochrome') {\n            let bright = 0.9;\n            let sat = 1.1;\n            for (let i = 0; i < this.items.length; i++) {\n                this.items[i].el.style.filter =\n                    `brightness(${bright}) ` + `saturate(${sat})`;\n                bright -= 0.1;\n                sat += 0.1;\n            }\n        } else if (this.colorMode === 'hues') {\n            let n = 45;\n            for (let i = 0; i < this.items.length; i++) {\n                this.items[i].el.style.filter = `hue-rotate(${n}deg)`;\n                n += 45;\n            }\n        } else {\n            console.log('Please set a color mode.');\n        }\n    }\n    toggleMenuItems() {\n        this.items.forEach((item) => {\n            item.toggleDisplay(this.itemsVisible);\n        });\n        this.itemsVisible = !this.itemsVisible;\n    }\n}\n\n\n\n\n//# sourceURL=webpack://nav-menus/./src/nav-menus.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
---
+++ src/nav-menus.js
@@ -0,0 +1,117 @@
+// Helper fn to toggle class.
+function toggleClass(el, cls) {
+    // Decide if complete class needs to be removed or added.
+    const action = el.classList.contains(cls) ? 'remove' : 'add';
+    // Remove/add the class.
+    el.classList[action](cls);
+}
+
+class MenuItem {
+    constructor(el, name, index) {
+        this.el = el;
+        this.name = name;
+        this.index = index;
+        this.measuredDimension = 'height';
+        this._distanceFromStart;
+    }
+    get distanceFromStart() {
+        const measurement = parseInt(
+            window.getComputedStyle(this.el)[this.measuredDimension]
+        );
+        return measurement * (this.index + 1);
+    }
+    toggleDisplay(isVisible) {
+        if (isVisible) {
+            this.hide();
+        } else {
+            this.show();
+        }
+        this.triggerFade();
+    }
+    show() {
+        this.el.style.transform = `translate(0, ${this.distanceFromStart}px)`;
+    }
+    hide() {
+        this.el.style.transform = 'translate(0, 0)';
+    }
+    triggerFade() {
+        if (
+            !this.el.classList.contains('fade-in') &&
+            !this.el.classList.contains('fade-out')
+        ) {
+            this.el.classList.add('fade-in');
+        } else {
+            toggleClass(this.el, 'fade-in');
+            toggleClass(this.el, 'fade-out');
+        }
+    }
+}
+
+class MobileMenuItem extends MenuItem {
+    constructor(el, name, index) {
+        super(el, name, index);
+        this.measuredDimension = 'width';
+    }
+    show() {
+        this.el.style.transform = `translate(${this.distanceFromStart}px, 0)`;
+    }
+}
+
+class Menu {
+    constructor(titleEl, colorMode, itemsArray) {
+        this.items = itemsArray || [];
+        this.titleEl = titleEl;
+        this.titleEl.addEventListener('click', () => this.toggleMenuItems());
+        this._colorMode = colorMode;
+        this.itemsVisible = false;
+    }
+    get colorMode() {
+        return this._colorMode;
+    }
+    set colorMode(mode) {
+        // Remove spaces and standardize case of string.
+        const uniformMode = mode.replace(/[\W_]/g, '').toLowerCase();
+        if (uniformMode !== 'monochrome' && uniformMode !== 'hues') {
+            alert('Invalid color mode. Please choose "monochrome" or "hues".');
+            return;
+        } else {
+            this._colorMode = uniformMode;
+        }
+    }
+    setZIndices() {
+        let z = this.items.length;
+        this.titleEl.style.zIndex = z + 1;
+        this.items.forEach((item) => {
+            item.el.style.zIndex = z;
+            z--;
+        });
+    }
+    setFilters() {
+        if (this.colorMode === 'monochrome') {
+            let bright = 0.9;
+            let sat = 1.1;
+            for (let i = 0; i < this.items.length; i++) {
+                this.items[i].el.style.filter =
+                    `brightness(${bright}) ` + `saturate(${sat})`;
+                bright -= 0.1;
+                sat += 0.1;
+            }
+        } else if (this.colorMode === 'hues') {
+            let n = 45;
+            for (let i = 0; i < this.items.length; i++) {
+                this.items[i].el.style.filter = `hue-rotate(${n}deg)`;
+                n += 45;
+            }
+        } else {
+            console.log('Please set a color mode.');
+        }
+    }
+    toggleMenuItems() {
+        this.items.forEach((item) => {
+            item.toggleDisplay(this.itemsVisible);
+        });
+        this.itemsVisible = !this.itemsVisible;
+    }
+}
+
+export { MenuItem, MobileMenuItem, Menu };
