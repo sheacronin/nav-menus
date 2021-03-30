a18f0654fb4f1c2e751fb57170eb6a3d6125243c
Shea Cronin <shealcronin@gmail.com>
2021-03-29 20:12:04 -0400

    Update CSS Selectors and README.md, wrap index.js in an IIFE that returns an array of all menu objects

--- README.md
+++ README.md
@@ -21,13 +21,15 @@ Use the following class names for elements you wish to include in your navigatio
 
 ### Mobile Menus
 
+Follow the same semantic structure as the drop-down menus, except asign the parent div the `.mobile-menu` class instead of the `.menu` class.
+
 ```html
-<div class="menu mobile-menu">
-    <button class="menu-title mobile-menu-item">Mobile Links</button>
+<div class="mobile-menu">
+    <button class="menu-title">Mobile Links</button>
     <ul>
-        <li class="menu-item mobile-menu-item">Item 1</li>
-        <li class="menu-item mobile-menu-item">Item 2</li>
-        <li class="menu-item mobile-menu-item">Item 3</li>
+        <li class="menu-item">Item 1</li>
+        <li class="menu-item">Item 2</li>
+        <li class="menu-item">Item 3</li>
     </ul>
 </div>
 ```
--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 import { MenuItem, MobileMenuItem, Menu } from './nav-menus';
 
-// Store all instances of menu class.
-const allMenus = document.querySelectorAll('.menu');
+const menus = (function createMenus() {
+    // Store all instances of menu class.
+    const menuElements = document.querySelectorAll('.menu, .mobile-menu');
 
-allMenus.forEach((menuEl) => {
-    const title = menuEl.querySelector('.menu-title');
-    // Create objects for each menu.
-    const menu = new Menu(title, 'hues');
-    const items = [...menuEl.querySelectorAll('.menu-item')];
-    items.forEach((itemEl) => {
+    const menus = [];
+
+    menuElements.forEach((menuEl) => {
+        const title = menuEl.querySelector('.menu-title');
+        // Create objects for each menu.
+        // The second parameter in the constructor takes 'hues' or 'monochrome'
+        // as an argument for the menu's color mode.
+        const menu = new Menu(title, 'hues');
         // Create objects for each menu item.
-        const ItemClass = itemEl.classList.contains('mobile-menu-item')
+        const ItemClass = menuEl.classList.contains('mobile-menu')
             ? MobileMenuItem
             : MenuItem;
-        const item = new ItemClass(
-            itemEl,
-            itemEl.textContent,
-            items.indexOf(itemEl)
-        );
-        menu.items.push(item);
+        const items = [...menuEl.querySelectorAll('.menu-item')];
+        items.forEach((itemEl) => {
+            const item = new ItemClass(
+                itemEl,
+                itemEl.textContent,
+                items.indexOf(itemEl)
+            );
+            menu.items.push(item);
+        });
+        // Set the z index style for each item.
+        menu.setZIndices();
+        menu.setFilters();
+        // Push each menu object into the menus array to be returned.
+        menus.push(menu);
     });
-    // Set the z index style for each item.
-    menu.setZIndices();
-    menu.setFilters();
-});
+    return menus;
+})();
+
+export { menus };
